6a9215a847d5612a7484eef7b34bc319a9269aef
Oleksii_S <lex.sydoruk@gmail.com>
2024-02-22 22:23:51 +0100

    Ydsaour branch is up to date with 'origin/test'.

--- src/js/fetchData.js
+++ src/js/fetchData.js
@@ -1,3 +1,12 @@
-async function fetchData(url) {
-  // axios //
+import axios from 'axios';
+
+ async function fetchData(url) {
+  try {
+    const response = async () => await axios.get(url);
+    const data = await response();
+    return data.data;
+  } catch {
+    console.log(error);
+  }
 }
+export default fetchData
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { fetchData } from './js/fetchData';
+// const result = await fetchData(
+//   'https://books-backend.p.goit.global/books/category-list'
+// );
+
+console.log(
+  fetchData('https://books-backend.p.goit.global/books/category-list')
+);
