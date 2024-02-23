6d530eab76ea08245b359633083b584a176d9157
Oleksii_S <lex.sydoruk@gmail.com>
2024-02-23 12:23:36 +0100

    .

--- src/js/fetchData.js
+++ src/js/fetchData.js
@@ -1,17 +1,8 @@
 import axios from 'axios';
 
-export const BASE_URL = 'https://books-backend.p.goit.global/books';
-
-export const endPoints = {
-  list: '/category-list',
-  topbooks: '/top-books',
-  category: '/category?category=',
-  id: '/{id}',
-};
-
-export async function fetchData(endpoint, selectedCategory = '') {
+export async function fetchData(baseUrl, endPoint, selectedCategory = '') {
   try {
-    const url = BASE_URL + endpoint + selectedCategory;
+    const url = baseUrl + endPoint + selectedCategory;
     const response = await axios.get(url);
     return response.data;
   } catch (error) {
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { fetchData } from './js/fetchData';
+
+const BASE_URL = 'https://books-backend.p.goit.global/books';
+
+const endPoints = {
+  list: '/category-list',
+  topbooks: '/top-books',
+  category: '/category?category=',
+  id: '/{id}',
+};
