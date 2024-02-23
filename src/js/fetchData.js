91bcfaeb36c03eea6cb38b93af586ff52c303b50
irynahamoum <111897707+irynahamoum@users.noreply.github.com>
2024-02-23 12:07:53 +0100

    adjusted fetch function

--- src/js/fetchData.js
+++ src/js/fetchData.js
@@ -1,29 +1,18 @@
 import axios from 'axios';
 
-const btn = document.querySelector('.test-btn');
+export const BASE_URL = 'https://books-backend.p.goit.global/books';
 
-btn.addEventListener('click', () => {
-  const selectedCategory = 'Trade Fiction Paperback';
-  // fetchData('category', selectedCategory);
-  // fetchData('topbooks');
-  // fetchData('id', '');
-  fetchData('list');
-});
-
-const BASE_URL = 'https://books-backend.p.goit.global/books';
-
-const endPoints = {
+export const endPoints = {
   list: '/category-list',
   topbooks: '/top-books',
   category: '/category?category=',
   id: '/{id}',
 };
 
-async function fetchData(endpoint, selectedCategory = '') {
+export async function fetchData(endpoint, selectedCategory = '') {
   try {
-    const url = BASE_URL + endPoints[endpoint] + selectedCategory;
+    const url = BASE_URL + endpoint + selectedCategory;
     const response = await axios.get(url);
-    console.log(response.data);
     return response.data;
   } catch (error) {
     console.error('Error fetching data:', error);
