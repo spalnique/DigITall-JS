96e0d1887981e63b22583147604d2fd00f0ed4cb
Zymbsis <Aleksandra.aleksashka1809@gmail.com>
2024-02-25 00:06:39 +0100

    update

--- src/js/createCategoryList.js
+++ src/js/createCategoryList.js
@@ -1,3 +1,6 @@
 export function createCategoryList(data) {
-  return Array.from(data, item => `<li class="sidebar-category-list-item">${item.list_name}</li>`).join('');
+  return Array.from(
+    data,
+    item => `<li class="sidebar-category-list-item">${item.list_name}</li>`
+  ).join('');
 }
