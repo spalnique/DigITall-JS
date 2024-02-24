0f9f4f59c695bd61d8f3d6782788cb6de5226a65
Oleksii_S <lex.sydoruk@gmail.com>
2024-02-24 18:48:34 +0100

    update

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import { createCategoryMarkup } from './js/createParentElementMarkup';
 const refs = {
   mainTitle: document.querySelector('.main-title'),
   mainCatWrap: document.querySelector('.category-list-wrapper'),
-  mainCatList: null,
+  sidebarCatList: null,
   catList: document.querySelector('.sidebar-category-list'),
   seeMoreButtons: null,
 };
@@ -19,63 +19,78 @@ const endPoints = {
   category: '/category?category=',
   id: '/{id}',
 };
+
 document.addEventListener('DOMContentLoaded', async () => {
   refs.mainTitle.textContent = 'Best Sellers Books';
 
-  const catListData = await fetchData(endPoints.list);
-  const catListMarkup = createCategoryList(catListData);
-  renderContent(refs.catList, catListMarkup);
+  try {
+    const topBookData = await fetchData(endPoints.topbooks);
+    const topBooksMarkup = topBookData
+      .map(x => {
+        const title = `<h2 class="category-title">${x.list_name}</h2>`;
+        const catMarkup = createCategoryMarkup(x.books, createBookMarkup);
+        const button = createButton('see-more-button', 'See more', x.list_name);
+        return title + catMarkup + button;
+      })
+      .join('');
+    renderContent(refs.mainCatWrap, topBooksMarkup);
+  } catch (error) {
+    console.log('Error getting top sellers book data:', error);
+  }
 
-  const topBookData = await fetchData(endPoints.topbooks);
-  const topBooksMarkup = topBookData
-    .map(x => {
-      const title = `<h2 class="category-title">${x.list_name}</h2>`;
-      const catMarkup = createCategoryMarkup(x.books, createBookMarkup);
-      const button = createButton('see-more-button', 'See more', x.list_name);
-      return title + catMarkup + button;
-    })
-    .join('');
-  renderContent(refs.mainCatWrap, topBooksMarkup);
+  try {
+    const catListData = await fetchData(endPoints.list);
+    const catListMarkup = createCategoryList(catListData);
+    renderContent(refs.catList, catListMarkup);
+  } catch (error) {
+    console.log('Error getting sidebar category list:', error);
+  }
 
-  refs.seeMoreButtons = document.querySelectorAll('.see-more-button');
-  refs.seeMoreButtons.forEach(x =>
-    x.addEventListener('click', async e => {
-      if (!e.target.dataset.category) return;
-      refs.mainTitle.textContent = e.target.dataset.category;
+  refs.catList.addEventListener('click', async e => {
+    if (e.target === e.currentTarget) return;
+    refs.mainTitle.textContent = e.target.textContent;
 
+    try {
       const selectedCatData = await fetchData(
         endPoints.category,
-        e.target.dataset.category
+        e.target.textContent
       );
       const selectedCatMarkup = createCategoryMarkup(
         selectedCatData,
         createBookMarkup
       );
       renderContent(refs.mainCatWrap, selectedCatMarkup);
+    } catch (error) {
+      console.log('Error getting selected category data:', error);
+    }
+  });
+
+  refs.seeMoreButtons = document.querySelectorAll('.see-more-button');
+  refs.seeMoreButtons.forEach(x =>
+    x.addEventListener('click', async e => {
+      if (!e.target.dataset.category) return;
+      refs.mainTitle.textContent = e.target.dataset.category;
+      try {
+        const selectedCatData = await fetchData(
+          endPoints.category,
+          e.target.dataset.category
+        );
+        const selectedCatMarkup = createCategoryMarkup(
+          selectedCatData,
+          createBookMarkup
+        );
+        renderContent(refs.mainCatWrap, selectedCatMarkup);
+      } catch (error) {
+        console.log('Error getting selected category data:', error);
+      }
     })
   );
 
-  refs.mainCatList = document.querySelectorAll('.category-list');
-  refs.mainCatList.forEach(x => {
+  refs.sidebarCatList = document.querySelectorAll('.category-list');
+  refs.sidebarCatList.forEach(x => {
     x.addEventListener('click', async e => {
       if (e.target === e.currentTarget) return;
-      console.log(e.target.dataset.id);
       // openModal(e.target.dataset.id);
     });
   });
-});
-
-refs.catList.addEventListener('click', async e => {
-  if (e.target === e.currentTarget) return;
-  refs.mainTitle.textContent = e.target.textContent;
-
-  const selectedCatData = await fetchData(
-    endPoints.category,
-    e.target.textContent
-  );
-  const selectedCatMarkup = createCategoryMarkup(
-    selectedCatData,
-    createBookMarkup
-  );
-  renderContent(refs.mainCatWrap, selectedCatMarkup);
 });
