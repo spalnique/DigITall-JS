d703ea18980d66356ef60f06da397a04615c6b61
Oleksii_S <lex.sydoruk@gmail.com>
2024-02-25 01:57:46 +0100

    updated

---
+++ src/js/cartDataHandler.js
@@ -0,0 +1,20 @@
+export function cartDataHandler(e, bookData) {
+  const id = bookData._id;
+  const cartDataArray = JSON.parse(localStorage.getItem('cart'))
+    ? JSON.parse(localStorage.getItem('cart'))
+    : [];
+  if (e.target.textContent.toLowerCase().includes('add')) {
+    cartDataArray.push(bookData);
+    localStorage.setItem('cart', JSON.stringify(cartDataArray));
+  } else {
+    const i = cartDataArray.findIndex(x => x._id === id);
+    cartDataArray.splice(i, 1);
+    localStorage.setItem('cart', JSON.stringify(cartDataArray));
+  }
+}
+
+export function checkCartData(bookData) {
+  const id = bookData._id;
+  const books = JSON.parse(localStorage.getItem(key)) || [];
+  return books.some(item => item._id === id);
+}
