c140a2860e3e623368c8690786abcef9e93af3bb
Oleksii_S <lex.sydoruk@gmail.com>
2024-02-25 13:54:29 +0100

    god save us all

--- src/js/donation-list.js
+++ src/js/donation-list.js
@@ -1,138 +1,17 @@
-// const foundations = [
-//   {
-//     title: 'Save the Children',
-//     url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
-//     img: null,
-//   },
-//   {
-//     title: 'Project HOPE',
-//     url: 'https://www.projecthope.org/country/ukraine/',
-//     img: null,
-//   },
-//   {
-//     title: 'UNITED24',
-//     url: 'https://u24.gov.ua/uk',
-//     img: null,
-//   },
-//   {
-//     title: 'International Medical Corps',
-//     url: 'https://internationalmedicalcorps.org/country/ukraine/',
-//     img: null,
-//   },
-//   {
-//     title: 'Medicins Sans Frontieres',
-//     url: 'https://www.msf.org/ukraine',
-//     img: null,
-//   },
-//   {
-//     title: 'RAZOM',
-//     url: 'https://www.razomforukraine.org/',
-//     img: null,
-//   },
-//   {
-//     title: 'Action against hunger',
-//     url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
-//     img: null,
-//   },
-//   {
-//     title: 'World vision',
-//     url: 'https://www.wvi.org/emergencies/ukraine',
-//     img: null,
-//   },
-//   {
-//     title: 'Serhiy Prytula Charity Foundation',
-//     url: 'https://prytulafoundation.org/en',
-//     img: null,
-//   },
-// ];
-
-// const donationContainerEl = document.querySelector('.donation-container');
-// console.log(donationContainerEl);
-
-// const donationContainerMarkup = foundations
-//   .map(
-//     (foundation, index) =>
-//       `<li class="donation-item">
-//           <a class="donation-link" href="${foundation.url}" target="_blank">
-//             <span class="donation-number">${(index + 1)
-//               .toString()
-//               .padStart(2, '0')}</span>
-//             <img class="donation-image" src="${foundation.img}" alt="" />
-//             <p class="donation-title">${foundation.title}</p>
-//           </a>
-//         </li>`
-//   )
-//   .join('');
-
-// donationContainerEl.insertAdjacentHTML('beforeend', donationContainerMarkup);
-const donation = [
-  {
-    title: 'Save the Children',
-    url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
-    img: './img/save-the-children.png',
-  },
-  {
-    title: 'Project HOPE',
-    url: 'https://www.projecthope.org/country/ukraine/',
-    img: './img/project-hope.png',
-  },
-  {
-    title: 'International Medical Corps',
-    url: 'https://internationalmedicalcorps.org/country/ukraine/',
-    img: './img/int-med-corps.png',
-  },
-  {
-    title: 'RAZOM',
-    url: 'https://www.razomforukraine.org/',
-    img: './img/razom.png',
-  },
-  {
-    title: 'Action against hunger',
-    url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
-    img: './img/act-against-hunger.png',
-  },
-  {
-    title: 'Serhiy Prytula Charity Foundation',
-    url: 'https://prytulafoundation.org/en',
-    img: './img/prytula.png',
-  },
-  {
-    title: 'Medicins Sans Frontieres',
-    url: 'https://www.msf.org/ukraine',
-    img: './img/msf.png',
-  },
-
-  {
-    title: 'World vision',
-    url: 'https://www.wvi.org/emergencies/ukraine',
-    img: './img/world-vision.png',
-  },
-  {
-    title: 'UNITED24',
-    url: 'https://u24.gov.ua/uk',
-    img: './img/united24.png',
-  },
-];
-
-const donationListElement = document.querySelector('.donation-list');
-const scrollButton = document.querySelector('.scroll-btn');
-const arrowIcon = document.querySelector('.arrow-btn');
-
 let startIndex = 0;
 let isForward = true;
 
 function donationTemplate(donation, index) {
-  function pad(num) {
-    return num < 10 ? '0' + num : num;
-  }
-  return `<li class="donation-list-name">
-                <p class="donation-name-number">${pad(index + 1)}</p>
+  return `<li class="sidebar-donation-list-name">
+                <p class="sidebar-donation-name-number">${(index + 1)
+                  .toString()
+                  .padStart(2, 0)}</p>
                 <a
-                  class="donation-name-link"
+                  class="sidebar-donation-name-link"
                   href="${donation.url}"
                   target="_blank"
                   ><img
-                    class="donation-name-logo"
+                    class="sidebar-donation-name-logo"
                     src="${donation.img}"
                     alt="${donation.title}"
                   />
@@ -146,20 +25,20 @@ function donationListTemplate(data) {
     .join('');
 }
 
-function renderDonations() {
-  const visibleDonations = donation.slice(startIndex, startIndex + 6);
+export function renderDonations(parentElement, donationData) {
+  const visibleDonations = donationData.slice(startIndex, startIndex + 6);
   const markup = donationListTemplate(visibleDonations);
-  donationListElement.innerHTML = markup;
+  parentElement.innerHTML = markup;
 }
 
-function toggleScrollDirection() {
+export function toggleScrollIconDirection(parentElement) {
   isForward = !isForward;
-  arrowIcon.style.transform = isForward ? 'rotate(0deg)' : 'rotate(180deg)';
+  parentElement.style.transform = isForward ? 'rotate(0deg)' : 'rotate(180deg)';
 }
 
-function scrollDonations() {
+export function scrollDonations(parentElement, donationData) {
   if (isForward) {
-    if (startIndex + 6 < donation.length) {
+    if (startIndex + 6 < donationData.length) {
       startIndex += 3;
     } else {
       startIndex = 0;
@@ -168,15 +47,8 @@ function scrollDonations() {
     if (startIndex - 3 >= 0) {
       startIndex -= 3;
     } else {
-      startIndex = donation.length - 6;
+      startIndex = donationData.length - 6;
     }
   }
-  renderDonations();
+  renderDonations(parentElement, donationData);
 }
-
-scrollButton.addEventListener('click', () => {
-  scrollDonations();
-  toggleScrollDirection();
-});
-
-renderDonations();
