2ecfd12184b6fb509d451f589e849124b758636a
spalnique <136997639+spalnique@users.noreply.github.com>
2024-02-26 22:53:23 +0000

    Deploying to gh-pages from @ spalnique/DigITall-JS@1f9311d22313dd05e5080c6205621032f8f1e08d 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,35 @@
-import{s as e}from"./assets/showHideFn-90ce2d31.js";import"./assets/vendor-389f7b34.js";e("header-nav-link-shop");
+import{d as _,a as B,b as M,j as A,c as C}from"./assets/showHideFn-74f7ef41.js";import"./assets/vendor-389f7b34.js";const c="",p=t=>{try{const s=localStorage.getItem(`${c?`${c}_`:""}${t}`);if(s)return JSON.parse(s);throw new Error(`I do not find ${c?`${c}_`:""}${t}`)}catch(s){return console.warn("[getLS]: ",s),null}},H=(t,s)=>{try{s===null?localStorage.removeItem(`${c?`${c}_`:""}${t}`):localStorage.setItem(`${c?`${c}_`:""}${t}`,JSON.stringify(s))}catch(n){return console.warn("[setLS]: ",n),null}},i={offset:0,count:3},L=()=>{const t=p("cart")||[];return t!=null&&t.length?t.slice(i.offset,i.offset+i.count):[]},P=t=>{const s=p("cart");return s!=null&&s.length?(i.offset=(t-1)*i.count,L()):[]};function o(t,s){const n=document.createElement("button");return n.classList.add("cart-pagination-btn"),n.textContent=t,n.onclick=s,n}const T=(t,s)=>{const n=p("cart")||[],e=Math.ceil(n.length/i.count),a=i.offset/i.count+1;if(e===1)return;if(e===0){s(0);return}const r=document.createElement("div");r.classList.add("cart-pagination-btns");const d=3,h=a>=d,$=a!==e&&e>d,u=o("<<",()=>{s(1)});u.disabled=a===1,u.disabled||u.classList.add("cart-shrink"),r.appendChild(u);const g=o("<",()=>{s(a-1)});g.disabled=a===1,g.disabled||g.classList.add("cart-shrink"),r.appendChild(g);const y=o("...",()=>{});h||y.setAttribute("style","display:none;"),r.appendChild(y);for(let l=1;l<=d;l++){const k=h?l+a-d+Number($):l,m=o(k,()=>{s(k)});l===1&&h&&m.setAttribute("style","display:none;"),l===d&&$&&m.setAttribute("style","display:none;"),a===k&&m.classList.add("pagination-btn-active"),r.appendChild(m)}const S=o("...",()=>{});$||S.setAttribute("style","display:none;"),r.appendChild(S);const b=o(">",()=>{s(a+1)});b.disabled=e===a,b.disabled||b.classList.add("cart-shrink"),r.appendChild(b);const f=o(">>",()=>{s(e)});f.disabled=e===a,f.disabled||f.classList.add("cart-shrink"),r.appendChild(f),t.appendChild(r)},x=t=>s=>{const e=p("cart").filter(a=>a._id!==s);H("cart",e.length>0?e:null),v(t,L())},D=t=>`<li class="cart-item-glow cart-item">
+          <img class="cart-image" src="${t.book_image}"/>
+          <div class="cart-book-section">
+            <h3 class="cart-book-title">${t.title}</h3>
+            <button class="cart-remove-book-btn" onclick="window.deleteCardFromLSHandler('${t._id}')">
+              <span class="cart-trash">
+                <span></span>
+                <i></i>
+              </span>
+            </button>
+            <p class="cart-book-category">${t.list_name}</p>
+            <p class="cart-book-description">
+             ${t.description}
+            </p>
+
+            <p class="cart-book-autor">${t.author}</p>
+            <div class="cart-book-links">
+              <a href=${t.buy_links[0].url} class="cart-amazon cart-amazon-dark">
+                <img src=${B} alt=${t.buy_links[0].name} />
+              </a>
+              <a
+                href=${t.buy_links[1].url}
+                class="cart-apple-books cart-apple-books-dark"
+              >
+                <img src=${M} alt=${t.buy_links[1].name} />
+              </a>
+            </div>
+          </div>
+        </li>`,I=()=>`<li class="cart-background">
+        <p class="cart-background-text">
+          This page is empty, add some books and proceed to order.
+        </p>
+        <img class="cart-bg-img" src="../img/png/cart-background.png" />
+      </li>`,z=t=>s=>{const n=P(s);w(t,n)},v=(t,s)=>{t.innerHTML=_(s,D),T(t,z(t))},w=(t,s)=>{if(!(s!=null&&s.length)){t.innerHTML=_([""],I);return}v(t,s)},F=t=>{p("cart"),w(t,L())};A("header-nav-link-shop");C.mainTitle.innerHTML="Shopping <span>List</span>";F(C.shopListWrap);window.deleteCardFromLSHandler=x(C.shopListWrap);
 //# sourceMappingURL=commonHelpers2.js.map
