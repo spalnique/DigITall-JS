d9139f4f5dab850a634432baf9610fa27e73ba01
Andrey Svyrskyi <akkad1v@gmail.com>
2024-02-26 06:55:24 +0200

    mob menu

---
+++ src/js/mob-menu.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', function () {
+  function createMenuContent() {
+    const menuContent = `
+        <div class="menu-header"></div>
+        <div class="menu-body">
+          <div class="menu-user-profile">
+            <p class="menu-user-name"></p>
+            <svg class="menu-user-fill" width="22" height="22">
+              <use href="./img/icons.svg#user-fill"></use>
+            </svg>
+          </div>
+          <ul id="menu-list" class="menu-nav-list">
+            <li class="menu-nav-item">
+              <a class="menu-nav-link menu-nav-link-home make-yellow" href="./index.html">Home</a>
+            </li>
+            <li class="menu-nav-item">
+              <a class="menu-nav-link menu-nav-link-shop shopping-link" href="../shopping-list.html">
+                Shopping List
+                <span class="shoping-test">
+                  <svg class="menu-nav-icon-shop" width="20" height="20">
+                    <use href="./img/icons.svg#uil-cart"></use>
+                  </svg>
+                </span>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="menu-log-out">
+          <button type="button" class="log-out">
+            Log out
+            <svg class="log-out-icon" width="20" height="20">
+              <use href="./img/icons.svg#arrow-narrow-right"></use>
+            </svg>
+          </button>
+        </div>
+      `;
+
+    return menuContent;
+  }
+
+  const menuContainer = document.querySelector('.menu-container');
+  const menuContent = createMenuContent();
+  menuContainer.innerHTML = menuContent;
+  const menuModal = basicLightbox.create(menuContainer, {
+    className: 'mob-menu-lightbox',
+  });
+  menuModal.show();
+});
+
+// const openMenuButton = document.querySelector('.open-menu-button');
+// openMenuButton.addEventListener('click', openMenu);
