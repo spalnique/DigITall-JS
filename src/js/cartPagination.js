e467bfb002cfacba7001d2f0aa9f5c372f3b0247
irynahamoum <111897707+irynahamoum@users.noreply.github.com>
2024-02-26 22:44:49 +0100

    update pagination

--- src/js/cartPagination.js
+++ src/js/cartPagination.js
@@ -70,8 +70,7 @@ export const getCardsByPage = page => {
 
 function createPaginationButton(label, changePagination) {
   const button = document.createElement('button');
-  button.classList.add('pagination-btn');
-  button.classList.add('shrink');
+  button.classList.add('cart-pagination-btn');
   button.textContent = label;
   button.onclick = changePagination; // Attach click event
   return button;
@@ -81,21 +80,116 @@ export const renderPagination = (domElement, changePagination) => {
   const cart = getLS('cart') || [];
 
   const pages = Math.ceil(cart.length / store.count);
-
+  const currentPage = store.offset / store.count + 1;
   if (pages === 1) return;
   if (pages === 0) {
     changePagination(0);
     return;
   }
   const paginationDiv = document.createElement('div');
-  paginationDiv.classList.add('pagination-btns');
+  paginationDiv.classList.add('cart-pagination-btns');
 
-  for (let index = 0; index < pages; index++) {
-    paginationDiv.appendChild(
-      createPaginationButton(index + 1, () => {
-        changePagination(index + 1);
-      })
-    );
+  const countPageBtn = 3;
+  const isStartDotsBtn = currentPage >= countPageBtn;
+
+  const isEndDotsBtn = currentPage !== pages && pages > countPageBtn;
+
+  // ======= render first arrow btns ======= //
+  const btn1 = createPaginationButton('<<', () => {
+    changePagination(1);
+  });
+
+  btn1.disabled = currentPage === 1;
+
+  if (!btn1.disabled) {
+    btn1.classList.add('cart-shrink');
   }
+
+  paginationDiv.appendChild(btn1);
+
+  // ======= render first arrow btns ======= //
+  const btn2 = createPaginationButton('<', () => {
+    changePagination(currentPage - 1);
+  });
+
+  btn2.disabled = currentPage === 1;
+
+  if (!btn2.disabled) {
+    btn2.classList.add('cart-shrink');
+  }
+
+  paginationDiv.appendChild(btn2);
+
+  // ======= render dots btns ======= //
+
+  const dotsBtnPrev = createPaginationButton('...', () => {});
+
+  if (!isStartDotsBtn) {
+    dotsBtnPrev.setAttribute('style', 'display:none;');
+  }
+
+  paginationDiv.appendChild(dotsBtnPrev);
+
+  // ======= render page btn ======= //
+
+  for (let index = 1; index <= countPageBtn; index++) {
+    const newPage = isStartDotsBtn
+      ? index + currentPage - countPageBtn + Number(isEndDotsBtn)
+      : index;
+
+    const pageBtn = createPaginationButton(newPage, () => {
+      changePagination(newPage);
+    });
+
+    if (index === 1 && isStartDotsBtn) {
+      pageBtn.setAttribute('style', 'display:none;');
+    }
+
+    if (index === countPageBtn && isEndDotsBtn) {
+      pageBtn.setAttribute('style', 'display:none;');
+    }
+
+    if (currentPage === newPage) {
+      pageBtn.classList.add('pagination-btn-active');
+    }
+
+    paginationDiv.appendChild(pageBtn);
+  }
+
+  // ======= render dots btns ======= //
+
+  const dotsBtn = createPaginationButton('...', () => {});
+
+  if (!isEndDotsBtn) {
+    dotsBtn.setAttribute('style', 'display:none;');
+  }
+
+  paginationDiv.appendChild(dotsBtn);
+
+  // ======= render next arrow btns ======= //
+  const btn3 = createPaginationButton('>', () => {
+    changePagination(currentPage + 1);
+  });
+
+  btn3.disabled = pages === currentPage;
+
+  if (!btn3.disabled) {
+    btn3.classList.add('cart-shrink');
+  }
+
+  paginationDiv.appendChild(btn3);
+
+  // ======= render last arrow btns ======= //
+  const btn4 = createPaginationButton('>>', () => {
+    changePagination(pages);
+  });
+
+  btn4.disabled = pages === currentPage;
+
+  if (!btn4.disabled) {
+    btn4.classList.add('cart-shrink');
+  }
+
+  paginationDiv.appendChild(btn4);
   domElement.appendChild(paginationDiv);
 };
