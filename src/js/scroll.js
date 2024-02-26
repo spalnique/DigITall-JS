fb1fbbcc4b618a3c8061be0f14717875a83d4473
AnnAnnetik <143535720+AnnAnnetik@users.noreply.github.com>
2024-02-26 15:10:35 +0200

    add button

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -2,9 +2,22 @@
 const buttonUp = document.querySelector('.button-up');
 
 buttonUp.addEventListener('click', scrollOnClick);
+
+window.addEventListener('scroll', trackScroll);
+
+function trackScroll() {
+  const offset = window.pageYOffset;
+  const coord = document.documentElement.clientHeight;
+  if (offset > coord) {
+    buttonUp.classList.add('button-up--show');
+  } else {
+    buttonUp.classList.remove('button-up--show');
+  }
+}
+
 function scrollOnClick() {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth" 
-  });
+  if (window.pageYOffset > 0) {
+    window.scrollBy(0, -75);
+    setTimeout(scrollOnClick, 0);
+  }
 }
