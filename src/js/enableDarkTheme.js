e672b4840e671613767b6f4a8613a71ee9be1cc9
Maymiro11 <143181180+Maymiro11@users.noreply.github.com>
2024-02-28 10:19:50 +0100

    lokalStorage
    Retrieving theme from local storage. Slow dark theme. Change icon favicon

--- src/js/enableDarkTheme.js
+++ src/js/enableDarkTheme.js
@@ -1,13 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let container = document.querySelector('body');
+    let toggler = document.querySelector('#themeToggle');
 
+    let savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+        container.classList.add(savedTheme);
+        toggler.checked = (savedTheme === 'dark-theme');
+    }
 
-let container = document.querySelector('body');
-let toggler = document.querySelector('#themeToggle');
+    toggler.addEventListener('click', toggleDarkTheme);
 
-toggler.addEventListener('click', toggleDarkTheme);
-
-function toggleDarkTheme(){
-    let classList = container.classList;
-    toggler.checked ? classList.add('dark-theme') : classList.remove('dark-theme');
-}
-
-
+    function toggleDarkTheme(){
+        let classList = container.classList;
+        if (toggler.checked) {
+            classList.add('dark-theme');
+            localStorage.setItem('theme', 'dark-theme');
+        } else {
+            classList.remove('dark-theme');
+            localStorage.setItem('theme', 'light-theme');
+        }
+    }
+});
